9db06876c50920c79648e14b21a969b4e2aa002c
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-07-11 14:54:50 -0300

    ADD: itens da header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Router, Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
-    
+    <a href="/">Home</a>
+    <a href="#budget">Orçamento</a>
+    <img src='./src/assets/LOGO.png' alt='Logo da Mega World Solutions, onde as letras estão em branco e escla decrescente de tamanho, dentro da representação de um sol nas cores azul e amarelo'/>
     </>
   )
 }
