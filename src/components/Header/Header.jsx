850ea7bcc3c10c331570d46169699ff7940cd7a0
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-07-13 19:15:42 -0300

    ADD: inclusão dos mixins de cor, fonte e flex

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,10 +10,16 @@ import Logo from "../../assets/LOGO.png";
 function Header() {
   return (
     <header className={S.headerStyle}>
-      <section className={S.menuSec}>
-        <a href="/">Home</a>
-        <a href="#budget">Orçamento</a>
-      </section>
+        <nav className={S.menuNav}>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#budget">Orçamento</a>
+            </li>
+          </ul>
+        </nav>
       <figure className={S.logoFig}>
         <img src={Logo} alt='Logo da Mega World Solutions, onde as letras estão em branco e escla decrescente de tamanho, dentro da representação de um sol nas cores azul e amarelo' />
       </figure>
