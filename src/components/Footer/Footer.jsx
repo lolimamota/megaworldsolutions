c191f70ac9e9447d50ea33deb0a8ef67377e8039
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-07-14 10:22:09 -0300

    FIX: cores, espaçamento e fontsize do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+//IMPORTS PRINCIPAIS
+
+// IMPORTS DE ESTILO
+import '../../global.scss';
+import S from './footer.module.scss'
 
 function Footer() {
   return (
-    <>
+    <footer className={S.footerStyle}>
       <p>Mega World Solutions - Todos os direitos reservados - 2025</p>
       <p>Desenvolvido por Lorrayne Mota</p>
-    </>
+    </ footer>
   )
 }
 
