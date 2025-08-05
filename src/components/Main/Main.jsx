e554823e0e4bf8a1857d80ecc9696b29ef8816ea
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-08-05 14:41:36 -0300

    CNG: add position nos elementos para colocar a header e o footer acima da main e manter o background no lugar certo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,14 +11,13 @@ import S from './main.module.scss';
 function Main() {
   return (
     <main className={S.styleMain}>
-      <div className={S.orange}>
-
-      </div>
-        <section>
+      <section className={S.orange}>
+        <div>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nisl velit, ultricies in semper non, pulvinar euismod nibh. Vivamus tincidunt dignissim urna, ac posuere augue porttitor vel. Mauris dapibus, erat a pulvinar pharetra, tellus lacus fringilla erat, non eleifend urna nunc eget metus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean a sem et nibh tempor vestibulum a tincidunt ex.
         </p>
-        </section>
+        </div>
+      </section>
       <section className='budget'>
         <form action='/budget' method='POST'>
           <label htmlFor="name">
