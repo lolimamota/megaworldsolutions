92c367c887245cd94473f09573933e49e96a64c1
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-08-13 18:54:57 -0300

    CNG: removi um espaço entre as linhs 9 e 10 da main, adc a seção do bg laranja e da imagem, definido o tamanho da grade

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
+//IMPORT DOS ESTILOS
+import '../../global.scss';
+import * as S from '../../components/Main/main.module.scss';
+
 function Main() {
   return (
     <main>
-      <section className="background">
-        <div className='orange'>
+      <section className={S.bgSection1}>
+        <div className={S.orangeSection1}>
 
         </div>
-
       </section>
       <section className="contentPrinciple">
 
