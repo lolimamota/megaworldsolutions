31b54b9e174e1ddc7f0c5056b0a25993b1be29ee
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-08-15 15:40:35 -0300

    FIX: paragrafo de aprensentação com alinhamentos

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,12 +9,10 @@ function Main() {
     <main>
       <section className={S.bgSection1}>
         <div className={S.orangeSection1}>
-
+          <p className={S.contentPrinciple}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sequi optio sed sit nisi tempore a qui repudiandae officia porro iusto cumque, aperiam tempora inventore voluptatum vel vitae, iure illum! Itaque delectus minus earum consequatur perspiciatis quos cumque labore saepe.</p>
         </div>
       </section>
-      <section className="contentPrinciple">
-
-      </section>
+      
       <section className="forms">
         
       </section>
