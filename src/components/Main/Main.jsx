59ba38832514cddaf1b8eb62da2ed17c2f2b7b19
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-08-15 20:02:54 -0300

    ADD: alinhamento do primeiro paragrafo da section de aprsentação

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -12,8 +12,7 @@ function Main() {
           <p className={S.contentPrinciple}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat sequi optio sed sit nisi tempore a qui repudiandae officia porro iusto cumque, aperiam tempora inventore voluptatum vel vitae, iure illum! Itaque delectus minus earum consequatur perspiciatis quos cumque labore saepe.</p>
         </div>
       </section>
-      
-      <section className="forms">
+      <section className={S.forms}>
         
       </section>
     </main>
