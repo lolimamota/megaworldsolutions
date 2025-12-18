b04171cef4807796d68745183f1620134377e80a
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-12-18 00:59:05 -0300

    ADD: alinhamento do fomulário da segunda section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,90 +19,101 @@ function Main() {
             </h2>
           </div>
           <h3 className={S.titleH3}>
-              Reduza sua conta de luz, valorize seu imóvel e invista em um futuro sustentável.
-              Fale agora com um dos nossos especialistas e descubra o projeto ideal para você.
-            </h3>
+            Reduza sua conta de luz, valorize seu imóvel e invista em um futuro sustentável.
+            Fale agora com um dos nossos especialistas e descubra o projeto ideal para você.
+          </h3>
           <div className={S.homeBtn}>
             <a href="#budget">Quero economizar!</a>
           </div>
         </div>
       </section>
       <section className={S.forms}>
-      <h2>
-        Preencha o cadastro e entraremos em contato com você o mais breve possível!
-      </h2>
-      <form action="" className={S.forms}>
-      {/* <form> */}
-        <label htmlFor="nome-sobrenome">Nome e sobrenome?
-          <input type="text" id='nome-sobrenome' name='nome-sobrenome' required/>
-        </label>
-        
+        <h2 className={S.subtitleForms}>
+          Preencha o cadastro e entraremos em contato com você o mais breve possível!
+        </h2>
+        <form action="" className={S.labels}>
+          <div className={S.divTeste}>
+            <label htmlFor="nome-sobrenome">Nome e sobrenome?
+            </label>
+            <input type="text" id='nome-sobrenome' name='nome-sobrenome' required />
+          </div>
 
-        <label htmlFor="telefone">Telefone de contato?
-          <input type="tel" id='telefone' name='telefone' pattern='\([0-9]{2}\) 9 [0-9]{4}-[0-9]{4}' required/>
-        </label>
-        
+          <div className={S.divTeste}>
+            <label htmlFor="telefone">Telefone de contato?
+            </label>
+            <input type="tel" id='telefone' name='telefone' pattern='\([0-9]{2}\) 9 [0-9]{4}-[0-9]{4}' required />
+          </div>
 
-        <label htmlFor="email">Email?
-          <input type="email" id='email' name='email' required/>
-        </label>
+          <div className={S.divTeste}>
+            <label htmlFor="email">Email?
+            </label>
+            <input type="email" id='email' name='email' required />
+          </div>
 
-        <label htmlFor="estado">Estado?
-          <input type="text" id='estado' name='estado' required />
-        </label>
+          <div className={S.divTeste}>
+            <label htmlFor="estado">Estado?
+            </label>
+            <input type="text" id='estado' name='estado' required />
+          </div>
 
-        <label htmlFor="cidade">Cidade?
-          <input type="text" id='cidade' name='cidade' required />
-        </label>
+          <div className={S.divTeste}>
+            <label htmlFor="cidade">Cidade?
+            </label>
+            <input type="text" id='cidade' name='cidade' required />
+          </div>
 
-        <label htmlFor="conta">Valor da última conta de luz?
-        <select name="conta" id="cidade" required>
-          <option value="null">
-            - Selecione um valor -
-          </option>
-          <option value="valor1">
-            R$ 250,00 até R$ 349,00
-          </option>
-          <option value="valor2">
-            De R$ 350,00 a R$499,00
-          </option>
-          <option value="valor3">
-            De R$500,00 a R$ 749,00
-          </option>
-          <option value="valor4">
-            De R$750,00 a R$999,00
-          </option>
-          <option value="valor5">
-            De R$1.000,00 a R$1.999,99
-          </option>
-          <option value="valor4">
-            De R$ 2.000,00 a R$5.000,00
-          </option>
-          <option value="valor5">
-            Acima de R$5.000,00
-          </option>
-        </select>
-        `</label>
+          <div className={S.divTeste}>
+            <label htmlFor="conta">Valor da última conta de luz?
+            </label>
+            <select name="conta" id="cidade" required>
+              <option value="null">
+                - Selecione um valor -
+              </option>
+              <option value="valor1">
+                R$ 250,00 até R$ 349,00
+              </option>
+              <option value="valor2">
+                De R$ 350,00 a R$499,00
+              </option>
+              <option value="valor3">
+                De R$500,00 a R$ 749,00
+              </option>
+              <option value="valor4">
+                De R$750,00 a R$999,00
+              </option>
+              <option value="valor5">
+                De R$1.000,00 a R$1.999,99
+              </option>
+              <option value="valor4">
+                De R$ 2.000,00 a R$5.000,00
+              </option>
+              <option value="valor5">
+                Acima de R$5.000,00
+              </option>
+            </select>
+          </div>
 
-        <label htmlFor="telhado">Qual o tipo do seu telhado?
-          <select name="telhado" id="telhado">
-            <option value="null">
-              - Selecione o tipo -
-            </option>
-            <option value="colonial">Telhado Colonial (Cerâmico)</option>
-            <option value="metálico">Telhado Metálico</option>
-            <option value="concreto">Telhado com telhas de concreto</option>
-            <option value="pvc">Telhado de PVC</option>
-            <option value="laje">Laje</option>
-            <option value="solo">Solo</option>
-            <option value="outro">
-              <label htmlFor="outro" id='outro'>Outro:
-              <input type="text-area" id='outro' required />
-              </label>
-            </option>
-          </select>
-        `</label>
-      </form>
+          <div className={S.divTeste}>
+            <label htmlFor="telhado">Qual o tipo do seu telhado?
+            </label>
+            <select name="telhado" id="telhado">
+              <option value="null">
+                - Selecione o tipo -
+              </option>
+              <option value="colonial">Telhado Colonial (Cerâmico)</option>
+              <option value="metálico">Telhado Metálico</option>
+              <option value="concreto">Telhado com telhas de concreto</option>
+              <option value="pvc">Telhado de PVC</option>
+              <option value="laje">Laje</option>
+              <option value="solo">Solo</option>
+              <option value="outro">
+                <label htmlFor="outro" id='outro'>Outro:
+                  <input type="text-area" id='outro' required />
+                </label>
+              </option>
+            </select>
+          </div>
+        </form>
       </section>
     </main>
   )
